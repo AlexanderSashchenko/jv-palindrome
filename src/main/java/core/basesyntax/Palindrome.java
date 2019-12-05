50fce5b5cda00be697955834b2f04aefc2055913
Alexandr Sashchenko <santys87@gmail.com>
2019-12-05 19:35:36 +0200

    Updated solution for Mate HW4.3

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,13 +21,11 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        String targetText = text.toLowerCase().replaceAll("[^a-z0-9]+","");
-        String reversedText = new StringBuilder(targetText).reverse().toString();
-        for (int i = 0; i < targetText.length(); i++) {
-            if (targetText.charAt(i) != reversedText.charAt(i)) {
-                return false;
-            }
+        String straightText = text.toLowerCase().replaceAll("[^a-z0-9]+","");
+        String reversedText = new StringBuilder(straightText).reverse().toString();
+        if (straightText.equals(reversedText)) {
+            return true;
         }
-        return true;
+        return false;
     }
 }
